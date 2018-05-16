d1e80e6898b01f5138657a9abbac76fd90979b2a
Mohamed Wassim DIMASSI <mwdimassi@dom.tti>
2018-05-16 14:26:07 +0100

    first commit

---
+++ src/main/java/com/test/web3j/demo/Web3jApplication.java
@@ -0,0 +1,13 @@
+package com.test.web3j.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web3jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web3jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/web3j/demo/Web3jApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.web3j.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Web3jApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
